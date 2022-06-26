fd241482874d5beacc85bf170d88ed2ef4aaf8f6
hmanjun <harshith.hm03@gmail.com>
2022-06-26 16:40:52 -0700

    Add simple styling

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var gameRunning = false
+const q1 = {
+    question: "This is a for loop where val takes each value in the array: for(val in array){}",
+    ansArr: ["True","False"],
+    ansIndex:1
+}
+
